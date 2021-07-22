80b03dba3dc0003bdd0fd7ec69b27d22f62f1281
Angela Maruca <angelamaruca@Angelas-MacBook-Pro.local>
2021-07-22 16:23:55 -0400

    cleaned scaffoled code, fixed up posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   post '/auth/login', to: 'authentication#login'
   get '/auth/verify', to: 'authentication#verify'
   resources :posts
-  resources :categories
-  resources :users
+  resources :categories, only: [:index, :show]
+  resources :users, only: :create
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
