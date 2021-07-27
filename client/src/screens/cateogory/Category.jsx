4ac8214ebbfc2efc194ffed02d1a5cb248f3100c
Angela Maruca <angelamaruca@Angelas-MacBook-Pro.local>
2021-07-27 17:25:03 -0400

    category css grid fixed

--- client/src/screens/cateogory/Category.jsx
+++ client/src/screens/cateogory/Category.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
-import { useParams } from 'react-router'
-import { Link } from 'react-router-dom'
-
-import './Category.css'
+import React from "react";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 
 export default function Category({ postList }) {
-  const { id } = useParams()
-  const posts = postList.filter(post => post.category_id === +id)
-  console.log(postList)
+  const { id } = useParams();
+  const posts = postList.filter((post) => post.category_id === +id);
+  console.log(postList);
   return (
-    <>
-      {posts && posts.map(post => {
-        return (
-          <>
-          <Link to={`/posts/${post.id}`}>
-          <div className="activity-box">
-            <p className='title'>Activity: {post.activity}</p>
-            <img className="category-image" src={post.img_url} alt="title" />
-            </div>
-            </Link>
+    <div className="activity-container">
+      {posts &&
+        posts.map((post) => {
+          return (
+            <>
+              <Link to={`/posts/${post.id}`}>
+                <div className="activity-card">
+                  <p className="title">Activity: {post.activity}</p>
+                  <img
+                    className="activity-image"
+                    src={post.img_url}
+                    alt="title"
+                  />
+                </div>
+              </Link>
             </>
-        )
-      })}
-  
-    </>
-  )
+          );
+        })}
+    </div>
+  );
 }
